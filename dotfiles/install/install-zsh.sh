c8c2b09f051073c94dbd1f9089efef961c2567ff
one2ndpiece <toranpu.card.yagiri@gmail.com>
2026-02-20 12:02:25 +0900

    再installができるようにした

--- dotfiles/install/install-zsh.sh
+++ dotfiles/install/install-zsh.sh
@@ -4,7 +4,11 @@ DOTFILES="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" >/dev/null 2>&1 && pwd )"
 ln -sf $DOTFILES/.zshenv $HOME/.zshenv && . $HOME/.zshenv
 #---------------------------------------------
 # oh-my-zshのインストール
-echo n| sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if [ -d "$ZSH" ]; then
+  echo "Oh My Zsh is already installed at $ZSH. Skipping installation."
+else
+  echo n| sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
 #---------------------------------------------
 touch $DOTFILES/.config/zsh/custom.zsh
 #---------------------------------------------
